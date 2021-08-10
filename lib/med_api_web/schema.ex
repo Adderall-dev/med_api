66485ec0699c5523f21ddb6095d78f4abf06fb6c
Edwin <50717284+Adderall-dev@users.noreply.github.com>
2021-08-10 03:47:37 +0200

    add mutation

--- lib/med_api_web/schema.ex
+++ lib/med_api_web/schema.ex
@@ -5,12 +5,23 @@ defmodule MedApiWeb.Schema do
   # import types
   import_types(MedApiWeb.Schema.Types)
 
+  #queres
   query do
 
     @desc "Get a list of all users"
     field :users, list_of(:user_type) do
       #resolver
       resolve(&Resolvers.UserResolver.users/3)
+    end
+  end
+
+  #mutations
+  mutation do
+    @desc "Register a new user"
+    field :register_user, type: :user_type do
+      # expect user to insert data of type :user_input_type which is defined in user_type.ex
+      arg(:input, non_null(:user_input_type))
+      resolve(&Resolvers.UserResolver.register_user/3)
     end
   end
 end
